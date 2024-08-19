6f65d22212c2277a6a718885fd61d0ed73d36944
Omar Faruk <faruk.eete@gmail.com>
2024-08-19 15:25:09 +0600

    first commit

---
+++ project 1/problem-1.js
@@ -0,0 +1,18 @@
+// Declare variables a and b
+let a = 10;
+let b = 5;
+
+// Perform addition
+let addition = a + b;
+console.log("Addition: " + addition); 
+// Perform subtraction
+let subtraction = a - b;
+console.log("Subtraction: " + subtraction); 
+
+// Perform multiplication
+let multiplication = a * b;
+console.log("Multiplication: " + multiplication); 
+
+// Perform division
+let division = a / b;
+console.log("Division: " + division); 
---
+++ project 1/problem-3.js
@@ -0,0 +1,11 @@
+// Declare variables num1 and num2
+let num1 = 5;
+let num2 = 10;
+
+// Concatenate num1 and num2 as strings
+let concatenation = num1.toString() + num2.toString();
+console.log("Concatenation: " + concatenation); 
+
+// Perform arithmetic addition on num1 and num2
+let addition = num1 + num2;
+console.log("Addition: " + addition); 
